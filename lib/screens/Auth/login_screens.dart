74cee89fef3bbe58c5e5a44d0fce477b170447ed
ren5602 <naziz5602@gmail.com>
2025-10-22 06:43:49 +0700

    edit readme, small padding reg

--- lib/screens/Auth/login_screens.dart
+++ lib/screens/Auth/login_screens.dart
@@ -18,7 +18,7 @@ class _LoginPageState extends State<LoginPage> {
   final _formKey = GlobalKey<FormState>();
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
-  bool _obscurePassword = false;
+  bool _obscurePassword = true;
 
   @override
   void dispose() {
